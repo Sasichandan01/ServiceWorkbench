27eab1a73a46bd20f969c613adaa620b629f46ff
Abhishek Suresh <abhishek.suresh@cloudwick.com>
2025-07-03 12:25:15 +0000

    Merged in wb-cb4-80 (pull request #48)
    [CB4]: RBAC mapping file,Roles table, Roles Lambda & Custom Resource
    
    * Added roles_lambda
    
    * Added new Lambda Layer and Roles Lambda Function
    
    * Added /roles in swagger - only put method
    
    * correct error response
    
    * Merged wb-abhishek into wb-cb4-80
    
    * Merged in wb-cb4-80 (pull request #53)[DEPLOY]
    
    Wb cb4 80
    
    * Added tags to roles lambda
    
    * Merged in wb-cb4-80 (pull request #55)[DEPLOY]
    
    Added tags to roles lambda
    
    * Merge branch 'develop' into wb-cb4-80
    
    * Refactor VPC configuration in deploy.yaml to use Fn::Sub for dynamic subnet and security group IDs
    
    * Merged in wb-cb4-80 (pull request #58)[DEPLOY]
    
    Wb cb4 80
    
    * Remove Name tag from Lambda function in deploy.yaml
    
    * Merged in wb-cb4-80 (pull request #59)[DEPLOY]
    
    Remove Name tag from Lambda function in deploy.yaml
    
    * Update deploy.yaml to use S3 for Lambda Layer content and add Name tag for WorkbenchV2
    
    * Merged wb-abhishek into wb-cb4-80
    
    * Remove Name tag from Lambda function in deploy.yaml
    
    * Add Lambda permission for API Gateway to invoke RolesLambdaFunction and change HTTP method for /roles endpoint to POST in swagger.yaml
    
    * Refactor roles_lambda.py to handle POST requests for role synchronization, improve error handling, and return appropriate status codes.
    
    * Add environment variables to RolesLambdaFunction in deploy.yaml for dynamic RolesTable reference
    
    * Enhance swagger.yaml by adding a query parameter for action in the /roles POST endpoint, updating the Lambda function URI, and implementing request validators for improved API request handling.
    
    * bug fix
    
    * Update swagger.yaml to include a required 'message' field in the error response component for improved API documentation.
    
    * Fix role handling in roles_lambda.py by updating error messages and modifying the role addition logic to use the 'Roles' key from the request body.
    
    * Update swagger.yaml to change the Lambda function URI to a placeholder for the RolesLambdaFunction and clean up whitespace in the request validators section.
    
    * Add RBAC custom resource and roles management Lambda functions
    rbac_custom_resource.py to handle role additions to the DynamoDB Roles Table.
    - Implemented roles_lambda.py for synchronizing system roles with improved error handling and logging.
    - Updated rbac.py to support role updates and batch processing.
    - Modified deploy.yaml and infra.yaml to include necessary IAM roles and Lambda layers.
    - Cleaned up swagger.yaml to reflect changes in API responses and request handling.
    
    
    Approved-by: Yadukrishnan Perumpillil

---
+++ Code/Lambda/RBACCustomResource/rbac_custom_resource.py
@@ -0,0 +1,39 @@
+import logging
+import json
+import boto3
+import botocore
+from RBAC.rbac import add_roles_to_table
+from CustomResource.custom_resource import send_cfn_response
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+DYNAMODB_RESOURCE = boto3.resource('dynamodb')
+
+def handler(event, context):
+    """
+    Custom resource to add roles to the Roles Table.
+    """
+    try:
+        # get the resource properties that are passed from cloudformation to lambda function
+        props = event.get('ResourceProperties', {})
+        roles_table = props.get('RolesTable')
+        table = DYNAMODB_RESOURCE.Table(roles_table)
+        # load the role permission mapping from the json file
+        with open('RBAC/role_permission_mapping.json', 'r', encoding='utf-8') as f:
+            role_mapping = json.load(f)
+        # add roles to the table in a batch writer
+        add_roles_to_table(table, role_mapping)
+        send_cfn_response(event, context, "SUCCESS")
+    except botocore.exceptions.ClientError as e:
+        LOGGER.exception("AWS ClientError: %s", e)
+        send_cfn_response(event, context, "FAILED", reason=str(e))
+    except KeyError as e:
+        LOGGER.exception("Missing key: %s", e)
+        send_cfn_response(event, context, "FAILED", reason=f"Missing key: {e}")
+    except TypeError as e:
+        LOGGER.exception("Type error: %s", e)
+        send_cfn_response(event, context, "FAILED", reason=str(e))
+    except Exception as e:
+        LOGGER.exception("Error in handler: %s", e)
+        send_cfn_response(event, context, "FAILED", reason=str(e))
