482d7499fae6ce18ebb19550323d4f2821dc6e24
Abhishek-Suresh <abhishek.suresh@cloudwick.com>
2025-07-03 13:55:10 +0530

    Refactor roles_lambda.py to handle POST requests for role synchronization, improve error handling, and return appropriate status codes.

--- Code/Roles/roles_lambda.py
+++ Code/Roles/roles_lambda.py
@@ -12,36 +12,44 @@ ROLES_TABLE = os.getenv('ROLES_TABLE')
 DYNAMODB_RESOURCE = boto3.resource('dynamodb')
 table = DYNAMODB_RESOURCE.Table(ROLES_TABLE)
 
-
 def handler(event, context):
     """
     Lambda function to get the roles from the table.
     """
-    LOGGER.info("Event: %s", event)
-    resource = event.get('resource')
-    method = event.get('httpMethod')
-    if resource == '/roles' and method == 'PUT':
-        body = event.get('body',{})
-        try:
-            body = json.loads(body)
-        except json.JSONDecodeError as e:
-            LOGGER.error("Error parsing request body: %s", e)
+    try:
+        LOGGER.info("Event: %s", event)
+        resource = event.get('resource')
+        method = event.get('httpMethod')
+        query_params = event.get('queryStringParameters', {})
+        action = query_params.get('action')
+        if resource == '/roles' and method == 'POST' and action == 'sync-role':
+            body = event.get('body',{})
+            try:
+                body = json.loads(body)
+            except json.JSONDecodeError as e:
+                LOGGER.error("Error parsing request body: %s", e)
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"Error": "Invalid request body"})
+                }
+            if not body.get('Role') or not body.get('Permissions'):
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"Error": "Role and Permissions are required"})
+                }
+            add_roles_to_table(table, body)
             return {
-                "statusCode": 400,
-                "body": json.dumps({"Error": "Invalid request body"})
+                "statusCode": 200,
+                "body": json.dumps({"Message": "Role added successfully"})
             }
-        if not body.get('Role') or not body.get('Permissions'):
+        else:
             return {
-                "statusCode": 400,
-                "body": json.dumps({"Error": "Role and Permissions are required"})
+                "statusCode": 404,
+                "body": json.dumps({"Error": "Not found"})
             }
-        add_roles_to_table(table, body)
+    except Exception as e:
+        LOGGER.exception("Error in handler: %s", e)
         return {
-            "statusCode": 200,
-            "body": json.dumps({"Message": "Role added successfully"})
-        }
-    else:
-        return {
-            "statusCode": 404,
-            "body": json.dumps({"Error": "Not found"})
+            "statusCode": 500,
+            "body": json.dumps({"Error": "Internal server error"})
         }
