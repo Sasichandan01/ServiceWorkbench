98c1098ed1353c03705d0d1394b08f6b5ecdaf85
Abhishek-Suresh <abhishek.suresh@cloudwick.com>
2025-07-07 17:14:04 +0530

    Implemented the is_user_action_valid function to check user permissions based on role and resource. The function fetches role permissions and API permission mappings, ensuring users have the necessary access levels. Added error handling for missing roles and permissions.

--- Code/Layer/RBAC/rbac.py
+++ Code/Layer/RBAC/rbac.py
@@ -6,6 +6,65 @@ from datetime import datetime
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
+level_rank = {'view': 1, 'manage': 2, 'fullaccess': 3}
+
+def is_user_action_valid(user_id, role, resource, method, table):
+    """
+    Function to check if the user action is valid.
+    1. Fetch Role permissions from the table
+    2. Fetch api permission mapping from the table
+    3. Check if the user has the permission to perform the action based on the hieracy of the permission view < manage < fullaccess
+    4. Return True if the user has the permission, False otherwise
+    """
+
+    # 1. Fetch Role permissions from the table
+    try:
+        item = table.get_item(Key={"Role": role})['Item']
+        if user_id not in item.get('Users', []):
+            return {
+                "statusCode": 403,
+                "body": json.dumps({"error": "User not found in the role"})
+            }
+        role_perms = item.get('Permissions', [])
+    except KeyError:
+        return {
+            "statusCode": 403,
+            "body": json.dumps({"error": "Role not found"})
+        }
+
+    # 2. Fetch api permission mapping from the table
+    api_permission_mapping = {}
+    with open('/opt/python/RBAC/api_permission_mapping.json', 'r', encoding='utf-8') as f:
+        api_permission_mapping = json.load(f)
+
+    api_permission = api_permission_mapping.get(resource, {}).get(method.upper(), [])
+
+    if not api_permission:
+        return {
+            "statusCode": 403,
+            "body": json.dumps({
+                "error": f"Permission not found for resource: {resource} and method: {method}"
+                })
+        }
+
+    for perm in api_permission:
+        try:
+            key, req_level = perm.split('.', 1)
+            req_rank = level_rank.get(req_level, 0)
+        except ValueError:
+            continue
+
+        for rp in role_perms:
+            rp = rp.strip().lower()
+            try:
+                perm_key, perm_level = rp.split('.', 1)
+            except ValueError:
+                continue
+
+            if perm_key == key and level_rank.get(perm_level, 0) >= req_rank:
+                return True
+    return False
+
 def sync_system_roles(table,roles_mapping=None):
     """
     Function to add roles to the table in a batch writer.
