9290ef36754480101392a86e94ea49c6eb33c183
Mayank Gupta <mayank.gupta@cloudwick.com>
2025-07-21 09:15:43 +0000

    Merged in wb-cb4-81 (pull request #352)
    Wb cb4 81
    
    * add
    
    * add [DEPLOY]
    
    * add [DEPLOY]
    
    * add [DEPLOY]
    
    * add
    
    * updated
    
    * updated
    
    * resolved merge conflicts
    
    * Merge branch 'wb-bhargav' into wb-cb4-55
    
    * updated
    
    * updated [DEPLOY]
    
    * [DEPLOY] resolved merge conflicts
    
    * Merge branch 'develop' into wb-cb4-19
    
    * resolved
    
    * updated
    
    * Merged in wb-cb4-19 (pull request #328)
    
    Wb cb4 19 [DEPLOY]
    
    * updated
    
    * Merge remote-tracking branch 'origin/wb-mayank' into wb-mayank
    
    * Updated
    
    * UPdfated
    
    * UPdfated
    
    * UPdfated
    
    * UPdfated
    
    * UPdfated
    
    * UPdfated
    
    
    Approved-by: Yadukrishnan Perumpillil

--- Code/Lambda/WorkbenchPostAuthLambda/WorkbenchPostAuth.py
+++ Code/Lambda/WorkbenchPostAuthLambda/WorkbenchPostAuth.py
@@ -9,6 +9,7 @@ LOGGER.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 ACTIVITY_LOGS_TABLE = dynamodb.Table(os.environ['ACTIVITY_LOGS_TABLE'])
+USERS_TABLE = dynamodb.Table(os.environ['USERS_TABLE'])
 
 def lambda_handler(event, context):
     """
@@ -35,6 +36,15 @@ def lambda_handler(event, context):
         LOGGER.info(f"Logging login for user {user_id}")
         ACTIVITY_LOGS_TABLE.put_item(Item=log_item)
         LOGGER.info(f"Login logged successfully for user {user_id}")
+
+        now_iso = str(datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S"))
+        LOGGER.info(f"Updating LastLoginTime for user {user_id}")
+        USERS_TABLE.update_item(
+            Key={'UserId': user_id},
+            UpdateExpression='SET LastLoginTime = :login_time',
+            ExpressionAttributeValues={':login_time': now_iso}
+        )
+        LOGGER.info(f"LastLoginTime updated successfully for user {user_id}")
     except Exception as e:
         LOGGER.error(f"Error logging login: {e}")
 
